4aca31ac1629a35859260c37b73146afa04f8809
Mateus Paz <54851249+mateusflorez@users.noreply.github.com>
2022-08-25 10:15:52 -0300

    Added links to AMS

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -10,7 +10,7 @@ class ContactsController < ApplicationController
 
   # GET /contacts/1
   def show
-    render json: @contact, include: [:kind, :address, :phones]
+    render json: @contact, include: [:kind]
   end
 
   # POST /contacts
--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -5,6 +5,9 @@ class ContactSerializer < ActiveModel::Serializer
   has_many :phones
   has_one :address
 
+  link(:self) {contact_url(object.id)}
+  link(:kind) {kind_url(object.kind.id)}
+
   def attributes(*args)
     h = super(*args)
     h[:birthdate] = object.birthdate.to_time.iso8601 unless object.birthdate.blank?
