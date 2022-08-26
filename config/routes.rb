3d543a500b6b6d30a1fbd3f964057a0154ff18be
Mateus Paz <54851249+mateusflorez@users.noreply.github.com>
2022-08-26 09:23:45 -0300

    phones route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   resources :kinds
   resources :contacts do
     resource :kind, only: [:show]
+    resource :kind, only: [:show], path: 'relationships/kind'
+
+    resource :phones, only: [:show]
+    resource :phones, only: [:show], path: 'relationships/kind'
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
