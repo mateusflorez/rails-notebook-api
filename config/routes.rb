39ea16f43a08428da914708c7df3bcd3527e7e11
Mateus Paz <54851249+mateusflorez@users.noreply.github.com>
2022-08-29 11:01:05 -0300

    phone CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,13 @@ Rails.application.routes.draw do
     resource :kind, only: [:show], path: 'relationships/kind'
 
     resource :phones, only: [:show]
-    resource :phones, only: [:show], path: 'relationships/kind'
+    resource :phones, only: [:show], path: 'relationships/phones'
+
+    resource :phone, only: [:update, :create, :destroy]
+    resource :phone, only: [:update, :create, :destroy], path: 'relationships/phones'
 
     resource :address, only: [:show, :update, :create, :destroy]
-    resource :address, only: [:show, :update, :create, :destroy], path: 'relationships/kind'
+    resource :address, only: [:show, :update, :create, :destroy], path: 'relationships/address'
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
